aac2936761b58ad211be1959f04950a1eef2d01a
Jeroen van Meeuwen (Kolab Systems) <vanmeeuwen@kolabsys.com>
2014-10-30 13:44:41 +0100

    Correct sys.path extension

--- source/conf.py
+++ source/conf.py
@@ -16,8 +16,13 @@ import sys, os, glob, imp
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), '../ext'))
-
+sys.path = [
+        os.path.join(
+                os.path.dirname(os.path.abspath(__file__)),
+                '..',
+                'ext'
+            )
+    ] + sys.path
 
 # -- General configuration -----------------------------------------------------
 
