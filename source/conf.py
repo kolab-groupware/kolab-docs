ea85c2cd0d37218171ddc8e992d33833325db677
Jeroen van Meeuwen (Kolab Systems) <vanmeeuwen@kolabsys.com>
2015-02-18 09:19:57 +0100

    Set version to 3.4

--- source/conf.py
+++ source/conf.py
@@ -60,14 +60,14 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Kolab Groupware'
-copyright = u'2013, Jeroen van Meeuwen'
+copyright = u'2011-2015, Jeroen van Meeuwen'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
 # built documents.
 #
 # The short X.Y version.
-version = '3.3'
+version = '3.4'
 # The full version, including alpha/beta/rc tags.
 release = ''
 
